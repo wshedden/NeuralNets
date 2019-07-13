5a79d10a20d14cfb1b430ab574e2b82612849f5b
wshedden <will.sh@protonmail.com>
2019-07-14 00:28:39 +0100

    Added methods

--- Network.cs
+++ Network.cs
@@ -15,19 +15,33 @@ namespace NeuralNets{
             for(int i = 0; i < layers.Length; i++){
                 double[] prevLayerValues = new double[layers[i-1].Neurons.Length];
                 Array.ForEach(NetMath.Range(layers[i-1].Neurons.Length), j => prevLayerValues[j] = layers[i-1].Neurons[j].Val);
-
                 for(int j = 0; j < layers[i].Neurons.Length; j++){
                     double dotSum = NetMath.DotProduct(prevLayerValues, layers[i].Neurons[j].Weights);
                     double activated = NetMath.Sigmoid(dotSum + layers[i].Neurons[j].Bias);
                     layers[i].Neurons[j].Val = activated;
                 }
-                
-                
             }
         }
 
         public void PrintNeuronValues(){
             Array.ForEach(layers, layer => layer.PrintNeuronValues());
+        }
+
+        private void SetInputs(double[] inputs){
+            Array.ForEach(NetMath.Range(inputs.Length), i => layers[0].Neurons[i].Val = inputs[i]);
+        }
+
+        private double[] GetOutputs(){
+            Layer lastLayer = layers[layers.Length-1];
+            double[] outputs = new double[lastLayer.Neurons.Length];
+            Array.ForEach(NetMath.Range(lastLayer.Neurons.Length), i => outputs[i] = lastLayer.Neurons[i].Val);
+            return outputs;
+        }
+
+        public double[] OutVal (double[] inputs){
+            SetInputs(inputs);
+            Propagate();
+            return GetOutputs();
         }
 
         
